4725ae91ee843bb46db30a4192699d379adb388a
Kenneth Pouncey <kjpou@pt.lu>
2021-12-31 10:54:20 +0100

    formatting

--- api/src/database/mongoDatabase.ts
+++ api/src/database/mongoDatabase.ts
@@ -7,22 +7,27 @@ import { config } from "../config/config.ts";
 class MongoDatabase {
   public client: MongoClient;
   private static instance: MongoDatabase;
-  private MONGO_URL: string;
+  private MONGO_URI: string;
   private DB_NAME: string;
   constructor() {
     // First check for environment override parameters
-    this.MONGO_URL = Deno.env.get("MONGO_URL") || config.MONGODB_URI;
-    this.DB_NAME = Deno.env.get("DB_NAME") || config.MONGODB_DATABASE_NAME;
+    this.MONGO_URI = Deno.env.get("MONGODB_URI") || config.MONGODB_URI;
+    this.DB_NAME = Deno.env.get("MONGODB_DATABASE_NAME") ||
+      config.MONGODB_DATABASE_NAME;
     this.client = {} as MongoClient;
   }
 
   connect() {
     log.info(
-      `Connecting to mongodb at ${this.MONGO_URL} db: ${this.DB_NAME} --`,
+      `Connecting to mongodb at ${this.MONGO_URI} db: ${this.DB_NAME} --`,
     );
-    const mongoClient = new MongoClient();
-    mongoClient.connect(this.MONGO_URL as string);
-    this.client = mongoClient;
+    try {
+      const mongoClient = new MongoClient();
+      mongoClient.connect(this.MONGO_URI as string);
+      this.client = mongoClient;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   get getDatabase() {
