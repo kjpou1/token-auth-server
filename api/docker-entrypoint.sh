165f6be65310f7ea5621595846f91c7e37c4b320
Kenneth Pouncey <kjpou@pt.lu>
2021-12-31 17:50:01 +0100

    Add a docker-entrypoint.sh script file to help with starting the app based on an environment variable
    - # ENTRYPOINT execution program (e.g. deno or denon)
    -  BACKEND_ENTRYPOINT=denon

---
+++ api/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+#--unstable --import-map=import_map.json 
+
+deno run -A ${DENO_RUN_OPTIONS} Drakefile.ts gen-secret
+
+deno run -A ${DENO_RUN_OPTIONS} Drakefile.ts seed
+
+echo "The Dockerfile ENTRYPOINT has been executed!"
+
+exec ${BACKEND_ENTRYPOINT} "$@"
