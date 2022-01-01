d9faa4f50866853dfaa6138851372f865f2726b3
Kenneth Pouncey <kjpou@pt.lu>
2022-01-01 07:56:05 +0100

    Remove the environment check as it is taken care of in `Dotenv` module

--- api/src/utils/utils.ts
+++ api/src/utils/utils.ts
@@ -26,44 +26,6 @@ function ensureSecretKeyFileExists() {
 }
 
 export function ensureEnvironment() {
-  // Import environment variables from the ".env" file
-  // and ensure all the variables are defined
-  dotenv.config({ safe: true, export: true });
-  const missingEnvVars = [];
-
-  // Double check that the the "MONGODB_URI" is defined
-  if (typeof Deno.env.get("MONGODB_URI") !== "string") {
-    missingEnvVars.push("MONGODB_URI");
-  }
-
-  // Double check that the the "JWT_SECRET_FILE" is defined
-  if (typeof Deno.env.get("JWT_SECRET_FILE") !== "string") {
-    missingEnvVars.push("JWT_SECRET_FILE");
-  }
-
-  // Double check that the the "JWT_ACCESS_TOKEN_EXP" is defined
-  if (typeof Deno.env.get("JWT_ACCESS_TOKEN_EXP") !== "string") {
-    missingEnvVars.push("JWT_ACCESS_TOKEN_EXP");
-  }
-  // Double check that the the "JWT_REFRESH_TOKEN_EXP" is defined
-  if (typeof Deno.env.get("JWT_REFRESH_TOKEN_EXP") !== "string") {
-    missingEnvVars.push("JWT_REFRESH_TOKEN_EXP");
-  }
-  // Double check that the the "JWT_REFRESH_TOKEN_EXP" is defined
-  if (typeof Deno.env.get("JWT_COOKIE_NAME") !== "string") {
-    missingEnvVars.push("JWT_COOKIE_NAME");
-  }
-  // If any environment variable is missing throw an error
-  if (missingEnvVars.length > 0) {
-    throw new Error(
-      `The following environment variables are missing: ${
-        missingEnvVars.join(
-          ", ",
-        )
-      }`,
-    );
-  }
-
   ensureSecretKeyFileExists();
 }
 
